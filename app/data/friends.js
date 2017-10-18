7a3159cd7d23b8498648c979866b9cb5990fdccd
Ed Dobias <mred2nice@aol.com>
2017-10-18 11:54:58 -0400

    initial files

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+//*****************************************************/
+// DATA                                                /
+// Below data will hold all of the friends.            / 
+// Initially we just set it equal to a "dummy" friend. /
+// But you could have it be an empty array as well.    /
+//*****************************************************/
+var friendArray = [];
+//**************************************************************************************/
+// Note how we export the array. This makes it accessible to other files using require. /
+//**************************************************************************************/
+module.exports = friendArray;
